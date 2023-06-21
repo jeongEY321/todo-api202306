1f1acd0d49b0518dee9da53110e3e5176b253258
jeongEY <kingship89@naver.com>
2023-06-21 13:24:14 +0900

    # 1. todo api 할 일 기능 완성

---
+++ src/main/java/com/example/todo/todoapi/service/TodoService.java
@@ -0,0 +1,80 @@
+package com.example.todo.todoapi.service;
+
+import com.example.todo.todoapi.dto.request.TodoCreateRequestDTO;
+import com.example.todo.todoapi.dto.request.TodoModifyRequestDTO;
+import com.example.todo.todoapi.dto.response.TodoDetailResponseDTO;
+import com.example.todo.todoapi.dto.response.TodoListResponseDTO;
+import com.example.todo.todoapi.entity.Todo;
+import com.example.todo.todoapi.repository.TodoRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+@Transactional
+public class TodoService {
+
+    private final TodoRepository todoRepository;
+
+    // 할 일 목록 조회
+    // 요청에 따라 데이터 갱신, 수정, 삭제, 등이 발생한 후
+    // 최신의 데이터 내용을 클라이언트에게 전달해서 렌더링 하기 위해
+    // 목록 리턴 메서드를 서비스에서 처리.
+    public TodoListResponseDTO retrieve() {
+        List<Todo> entityList = todoRepository.findAll();
+
+        List<TodoDetailResponseDTO> dtoList = entityList.stream()
+                .map(todo -> new TodoDetailResponseDTO(todo))
+                .collect(Collectors.toList());
+
+        return TodoListResponseDTO.builder()
+                .todos(dtoList)
+                .build();
+    }
+
+
+    //할 일 삭제
+    public TodoListResponseDTO delete(final String todoId) {
+        try {
+            todoRepository.deleteById(todoId);
+        } catch (Exception e) {
+            log.error("id가 존재하지 않아 삭제에 실패했습니다. - ID: {}, err: {}", todoId, e.getMessage());
+            throw new RuntimeException("id가 존재하지 않아 삭제에 실패했습니다.");
+        }
+        return retrieve();
+    }
+
+    //할 일 등록
+    public TodoListResponseDTO create(final TodoCreateRequestDTO dto) throws RuntimeException {
+
+        todoRepository.save(dto.toEntity());
+        log.info("할 일 저장 완료! 제목: {}", dto.getTitle());
+        return retrieve();
+    }
+
+    //할 일 수정
+    public TodoListResponseDTO update(final TodoModifyRequestDTO dto) throws RuntimeException {
+
+        Optional<Todo> targetEntity = todoRepository.findById(dto.getId());
+
+        targetEntity.ifPresent(entity -> {
+            entity.setDone(dto.isDone());
+
+            todoRepository.save(entity);
+        });
+
+        return retrieve();
+
+    }
+}
